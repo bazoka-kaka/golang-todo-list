37b359d973cc8cd1f6a38716b4102bec12f3adbe
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-12-13 16:12:53 +0700

    add routing main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/bazoka-kaka/golang-todo-list/controllers"
+	"github.com/bazoka-kaka/golang-todo-list/middlewares"
+)
+
+func main() {
+	http.Handle("/register", middlewares.AllowOnlyPOST(http.HandlerFunc(controllers.HandleRegister)))
+	http.Handle("/login", middlewares.AllowOnlyPOST(http.HandlerFunc(controllers.HandleLogin)))
+	http.Handle("/logout", middlewares.AllowOnlyGET(http.HandlerFunc(controllers.HandleLogout)))
+
+	// api
+	http.Handle("/users", middlewares.Authenticate(middlewares.AllowOnlyGET(http.HandlerFunc(controllers.GetAllUsers))))
+
+	// listen to server
+	fmt.Println("server running on port 35000")
+	http.ListenAndServe(":3500", nil)
+}
